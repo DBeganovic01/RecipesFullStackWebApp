bfda6d36b2b53e54ed3e895acf05c1056599e85b
DBeganovic01 <dbeganovic7@gmail.com>
2022-08-03 16:58:59 -0400

    More bootstrap styling, added a Pantry route for ingredients

--- app.js
+++ app.js
@@ -11,17 +11,17 @@ app.use(bodyParser.urlencoded({extended: true}));
 
 const recipes = [
     {
-        title: "Baked Potato",
+        recipeName: "Baked Potato",
         description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
         image: "https://www.simplyrecipes.com/thmb/GZwoVSMKQ2tiBKner6bDmCXJPi0=/1600x1067/filters:fill(auto,1)/Twice-Baked-Potatoes-LEAD-1v2-f7918849ddd245c5aecf0ec58fb2b47e.jpg"
     },
     {
-        title: "Crepes",
+        recipeName: "Crepes",
         description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
         image: "https://handletheheat.com/wp-content/uploads/2014/02/How-to-Make-Crepes-SQUARE.jpg"
     },
     {
-        title: "Baked Salmon",
+        recipeName: "Baked Salmon",
         description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
         image: "https://www.acouplecooks.com/wp-content/uploads/2020/01/Broiled-Salmon-006-368x368.jpg"
     }
@@ -43,6 +43,10 @@ app.post("/recipes", (req, res) => {
 
 app.get("/recipes/new", (req, res) => {
     res.render("recipes_new");
+})
+
+app.get("/pantry", (req,res) => {
+    res.render("pantry");
 })
 
 app.listen(port, hostname, () => {
