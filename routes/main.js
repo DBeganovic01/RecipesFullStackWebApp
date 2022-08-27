587d6da29ef75e638d3fc09c6a30298b4b8eaaf0
DBeganovic01 <dbeganovic7@gmail.com>
2022-08-26 21:02:29 -0400

    Create account route, create isLoggedIn middleware to test authorization

--- routes/main.js
+++ routes/main.js
@@ -3,6 +3,18 @@ const router = express.Router();
 
 router.get("/", (req, res) => {
     res.render("landing");
-})
+});
+
+router.get("/account", isLoggeIn, (req, res) => {
+    res.render("account");
+});
+
+function isLoggeIn(req, res, next){
+    if (req.isAuthenticated()) {
+        return next();
+    } else {
+        res.redirect("/login");
+    }
+};
 
 module.exports = router;
